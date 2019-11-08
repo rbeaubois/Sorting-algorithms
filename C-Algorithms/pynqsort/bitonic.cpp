4d8d635eccb8d308847fcf3087ef095e9decb8ce
Mathieu PILLET <math.pillet@hotmail.com>
2019-11-08 10:49:48 +0100

    Complete readme

---
+++ C-Algorithms/pynqsort/bitonic.cpp
@@ -0,0 +1,8 @@
+#include <omp.h>
+
+#include "bitonic.hpp"
+
+inline unsigned int* bitonic_sort(unsigned int* arr, unsigned int arrsize) {
+
+	return arr;
+}
--- Data/Data_converter/build/data_converter.d
+++
@@ -1,3 +0,0 @@
-build/data_converter.o: src/data_converter.cpp src/fileManager.h
-
-src/fileManager.h:
