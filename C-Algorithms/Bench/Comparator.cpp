30dcf5be48420272bfe3da1b991a2ecd8a9df6e0
dubicube <49446589+dubicube@users.noreply.github.com>
2019-11-08 17:00:51 +0700

    Updated Comparator

--- C-Algorithms/Bench/Comparator.cpp
+++ C-Algorithms/Bench/Comparator.cpp
@@ -1,5 +1,8 @@
 #include "Comparator.h"
 
+#include <chrono>
+using namespace std;
+
 int Comparator::process(unsigned int d[], int len) {
     auto start = chrono::steady_clock::now();
     sort(d, len);
--- C-Algorithms/Bench/Comparator.h
+++ C-Algorithms/Bench/Comparator.h
@@ -1,10 +1,6 @@
 #ifndef COMPARATOR_H
 #define COMPARATOR_H
 
-#include <iostream>
-#include <chrono>
-#include <unistd.h>
-
 class Comparator {
 private:
     virtual unsigned int* sort(unsigned int data[], int len);
