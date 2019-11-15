248858a8acfaab031893678377277c1b02298252
Denis Shemonaev <denishem.shemonaev@gmail.com>
2019-11-15 10:05:17 +0100

    file2tab with vectors

---
+++ C-Algorithms/Insertion_sort/src/file2tab.h
@@ -0,0 +1,23 @@
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+class file2tab
+{
+private:
+    string filename;
+    vector<unsigned int> tab;
+    int nb_to_show;
+public:
+    file2tab(string filename, int nb, int tabl[]);
+    ~file2tab();
+    void printTab();
+    void setLen(int nb_to_show);
+    void vector2tab(unsigned int tab[]);
+    int getLen();
+};
+
+
--- C-Algorithms/Insertion_sort/src/tab.h
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <iostream>
-
-void TabFromFile(const char *filename, int len, unsigned int tab[]);
-void AfficherTab(int tab[], int n);
