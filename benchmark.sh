b2009f3f79d89a0ea54be374ca9eabb8736fbd31
Denis Shemonaev <denishem.shemonaev@gmail.com>
2020-01-10 11:31:13 +0100

    Enhanced benchmark

--- benchmark.sh
+++ benchmark.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 clear
-
+rm bench_results/plot.gnu
 good_sort=false;
 
 while [ "$good_sort" == false ]
@@ -22,6 +22,11 @@ echo "Enter maximum amount of data"
 echo
 read dat_nb
 
-./c/main $sort bench $dat_nb
-
+cd c
+./main "$sort" bench "$dat_nb"
+cd ..
+touch plot.gnu
+cat bench_results/plt_tmp.txt >> bench_results/plot.gnu
+echo -e "plot \"${sort}_results.csv\" using 1:2 title '${sort} sort' with line, \"${sort}_results.csv\" using 1:3 title 'Max duration' with line, \"${sort}_results.csv\" using 1:4 title 'Min duration' with line
+pause -1" >> bench_results/plot.gnu
 cd bench_results && gnuplot plot.gnu
