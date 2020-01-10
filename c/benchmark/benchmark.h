5574e74169001e63d88072c1448d58011a7e07d7
Romain Beaubois <captain.ulxus@gmail.com>
2020-01-10 12:29:11 +0100

    Add run for different data randomized, progress bar, info run

--- c/benchmark/benchmark.h
+++ c/benchmark/benchmark.h
@@ -11,12 +11,15 @@ using namespace std;
 
 #include "../comparator.h"
 
-#define AVERAGING   10
-#define DATA_STEP   128
+#define AVERAGING   25	// Number of runs for a fixed number of data
+#define DATA_STEP   256	// Increase step for the number of data
+#define NB_SETS		10	// Number of data sets to run
 
 // Randomize an array of size n
 	void randomize(unsigned int* data, int n);
 // Benchmarking a sort algorithm
 	void runBenchmark(Comparator *s, unsigned int* data, int n, string sort);
+// Print progress
+	void print_progress(unsigned int timer, int n);
 
 #endif
