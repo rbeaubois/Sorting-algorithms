aeec5c316d16bd41d15d19a093dc2db6c76ad3c6
Denis Shemonaev <denishem.shemonaev@gmail.com>
2020-01-10 16:41:18 +0100

    Test

--- bench_results/plot.gnu
+++ bench_results/plot.gnu
@@ -1,11 +1,16 @@
 set datafile separator ";"
+set term png size 1920,1080
+set output 'quick.png'
 set xlabel 'Number of elements'
 set ylabel 'Execution time (us)'
 set grid
 set key box
-set title "Benchmark results"
 set key height 1
 set key top left
 set key box spacing 1.5
-plot "bubble_results.csv" using 1:2 title 'bubble sort' with line, "bubble_results.csv" using 1:3 title 'Max duration' with line, "bubble_results.csv" using 1:4 title 'Min duration' with line
-pause -1
+set multiplot layout 1,2
+set title "CPU Benchmark results"
+plot "quick_results.csv" using 1:2 title 'quick sort' with line, "quick_results.csv" using 1:3 title 'quick max duration' with line, "quick_results.csv" using 1:4 title 'quick min duration' with line
+set title "PYNQ CPU Benchmark results"
+plot "quick_results_pynq.csv" using 1:2 title 'quick sort' with line, "quick_results_pynq.csv" using 1:3 title 'quick max duration' with line, "quick_results_pynq.csv" using 1:4 title 'quick min duration' with line
+unset multiplot
--- benchmark.sh
+++ benchmark.sh
@@ -27,6 +27,24 @@ cd c
 cd ..
 touch plot.gnu
 cat bench_results/plt_tmp.txt >> bench_results/plot.gnu
-echo -e "plot \"${sort}_results.csv\" using 1:2 title '${sort} sort' with line, \"${sort}_results.csv\" using 1:3 title 'Max duration' with line, \"${sort}_results.csv\" using 1:4 title 'Min duration' with line
-pause -1" >> bench_results/plot.gnu
-cd bench_results && gnuplot plot.gnu
+sed -i -e "s/image/$sort/g" bench_results/plot.gnu
+echo -e "set title \"CPU Benchmark results\"" >> bench_results/plot.gnu
+echo "Choose what to plot:"
+select plot_choice in Min Max All; do
+    case $plot_choice in
+        Min ) echo -e "plot \"${sort}_results.csv\" using 1:4 title '${sort} min duration' with line" >> bench_results/plot.gnu;
+		echo -e "set title \"PYNQ CPU Benchmark results\"" >> bench_results/plot.gnu;
+		echo -e "plot \"${sort}_results_pynq.csv\" using 1:4 title '${sort} min duration' with line" >> bench_results/plot.gnu; break;;
+        Max ) echo -e "plot \"${sort}_results.csv\" using 1:3 title '${sort} max duration' with line" >> bench_results/plot.gnu;
+		echo -e "set title \"PYNQ CPU Benchmark results\"" >> bench_results/plot.gnu;
+		echo -e "plot \"${sort}_results_pynq.csv\" using 1:3 title '${sort} max duration' with line" >> bench_results/plot.gnu; break;;
+		All ) echo -e "plot \"${sort}_results.csv\" using 1:2 title '${sort} sort' with line, \"${sort}_results.csv\" using 1:3 title '${sort} max duration' with line, \"${sort}_results.csv\" using 1:4 title '${sort} min duration' with line" >> bench_results/plot.gnu;
+		echo -e "set title \"PYNQ CPU Benchmark results\"">> bench_results/plot.gnu;
+		echo -e "plot \"${sort}_results_pynq.csv\" using 1:2 title '${sort} sort' with line, \"${sort}_results_pynq.csv\" using 1:3 title '${sort} max duration' with line, \"${sort}_results_pynq.csv\" using 1:4 title '${sort} min duration' with line" >> bench_results/plot.gnu;
+		 break;;
+    esac
+done
+
+echo -e "unset multiplot" >> bench_results/plot.gnu
+cd bench_results && gnuplot plot.gnu && feh $sort.png
+
