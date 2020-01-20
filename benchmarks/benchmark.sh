929e571d544525fdefd99cb115ab2bd1ebc5fe73
Denis Shemonaev <denishem.shemonaev@gmail.com>
2020-01-20 20:57:15 +0100

    fixed benchmark script

--- benchmarks/benchmark.sh
+++ benchmarks/benchmark.sh
@@ -18,11 +18,10 @@ do
 	fi
 done
 
-dat_nb = 65536
+dat_nb=65536
 
-cd c
-./main "$sort" bench "$dat_nb"
-cd ..
+../c/main "$sort" bench $dat_nb
+
 info="$(awk 'NR==1' ${sort}_results.csv)"
 touch plot.gnu
 cat plt_tmp.txt >> plot.gnu
@@ -46,5 +45,5 @@ select plot_choice in Min Max All; do
 done
 
 echo -e "unset multiplot" >> plot.gnu
-cd benchmarks && gnuplot plot.gnu && feh $sort.png
+gnuplot plot.gnu && feh ../bench_results/$sort.png
 
